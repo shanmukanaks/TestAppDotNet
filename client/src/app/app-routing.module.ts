54bc8dc14fffd1ecf321580373172559b4b0c922
Shanmu Kanagaraj <shanmukanaks@gmail.com>
2022-07-03 18:35:10 -0500

    routing pt1

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListsComponent } from './lists/lists.component';
+import { MembersDetailComponent } from './members/members-detail/members-detail.component';
+import { MembersListComponent } from './members/members-list/members-list.component';
+import { MessagesComponent } from './messages/messages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'members', component: MembersListComponent },
+  { path: 'members/:id', component: MembersDetailComponent },
+  { path: 'lists', component: ListsComponent },
+  { path: 'messages', component: MessagesComponent },
+  { path: '**', component: HomeComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -12,7 +13,7 @@ export class NavComponent implements OnInit {
 
   model: any = {}
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -20,6 +21,7 @@ export class NavComponent implements OnInit {
   login() 
   {
     this.accountService.login(this.model).subscribe(response => {
+      this.router.navigateByUrl('/members')
       console.log(response);
     }, error => {
       console.log(error);
@@ -29,6 +31,7 @@ export class NavComponent implements OnInit {
   logout() 
   { 
     this.accountService.logout();
+    this.router.navigateByUrl('/')
   }
 
 }
